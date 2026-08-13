3ac3fa9632b319042bc9a4c9eb6bb92cfdcf9103
SuryaMahathi <panthalamahathi@gmail.com>
2026-08-13 22:47:29 +0530

    Add files via upload

---
+++ week-1/annotations.js
@@ -0,0 +1,7 @@
+let studentName = "Mahathi";
+let rollNo = 101;
+let isPresent = true;
+console.log("Student Name:", studentName);
+console.log("Roll Number:", rollNo);
+console.log("Present:", isPresent);
+export {};
---
+++ week-1/annotations.ts
@@ -0,0 +1,8 @@
+let studentName: string = "Mahathi";
+let rollNo: number = 101;
+let isPresent: boolean = true;
+
+console.log("Student Name:", studentName);
+console.log("Roll Number:", rollNo);
+console.log("Present:", isPresent);
+export {};
